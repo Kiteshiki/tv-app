50324c770a2747375d863420fe4e419af4b38c29
S. Idris <idris.feltz@gmail.com>
2021-08-16 20:58:38 +0300

    Added loader component

---
+++ src/containers/loader/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './style.module.css';
+
+export default function Loader(): JSX.Element {
+    return (
+        <div className={styles.ldsRoller}>
+            <div />
+            <div />
+            <div />
+            <div />
+            <div />
+            <div />
+            <div />
+            <div />
+        </div>
+    );
+}
