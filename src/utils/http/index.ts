f711de6f2761244f4a4ed0cf792e2f51a89b3183
S. Idris <idris.feltz@gmail.com>
2021-08-24 20:33:40 +0300

    Added baseURL into .env file

--- src/utils/http/index.ts
+++ src/utils/http/index.ts
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://lab-gcp.ifeelsmart.net/demo/v1/data';
+axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
 axios.defaults.headers['Content-Type'] = 'application/json';
-axios.defaults.headers.post['Access-Control-Allow-Origin'] = 'lab-gcp.ifeelsmart.net';
 
 export default axios;
